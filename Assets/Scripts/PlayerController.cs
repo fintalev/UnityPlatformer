4da69844fe97a77f957fb4a45a515a4a28c8b309
fintalev <112973569+fintalev@users.noreply.github.com>
2024-01-15 12:16:16 +0100

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float maxSpeed=7;
+    [SerializeField] private float jumpHeight =150;
+    [SerializeField] private Transform groundChecker;
+    [SerializeField] private LayerMask groundLayer;
     private Animator animator;
     private Rigidbody2D rigidbody2d;
 
     private bool isFacingRight;
 
+    private bool isGrounded;
     
     private void Start()
     {
         isFacingRight=true;
+        isGrounded = false;
         animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
-    
+    private void Update()
+    {
+            if (isGrounded == true && Input.GetAxis("Jump") > 0)
+            {
+                isGrounded=false;
+                animator.SetBool("IsGrounded", false);
+                rigidbody2d.AddForce(new(0,jumpHeight));
+            }
+    }
+
+
     private void FixedUpdate()
     {
         float move = Input.GetAxis("Horizontal");
@@ -29,11 +44,16 @@ public class PlayerController : MonoBehaviour
         {
             Flip();
         }
+
+        isGrounded = Physics2D.OverlapCircle(groundChecker.position, .1f, groundLayer);
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("VerticalSpeed",rigidbody2d.velocity.y);
+
     }
 
     private void Flip()
     {
         isFacingRight = !isFacingRight;
-        transform.localScale = new(x: transform.localScale.x * -1, y: transform.localScale.y);
+        transform.localScale = new(x: transform.localScale.x * -1, y: transform.localScale.y, z:transform.localScale.z);
     }
 }
